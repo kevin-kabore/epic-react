ac42e8bebe51a7a6420f3d04b66f9f3c773d629d
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-03-17 16:53:37 -0700

    WIP feat(react-hooks): solution to http request effects, and EC 1

--- react-hooks/src/exercise/06.solution.js
+++ react-hooks/src/exercise/06.solution.js
@@ -3,7 +3,6 @@
 
 import * as React from 'react'
 // 🐨 you'll want the following additional things from '../pokemon':
-import { fetchPokemon, PokemonInfoFallback, PokemonDataView } from '../pokemon';
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
@@ -11,40 +10,22 @@ import {PokemonForm} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error, setError] = React.useState(null)
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
-  React.useEffect(() => {
-    if (!pokemonName) return;
-    // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-    setPokemon(null)
-    
-    fetchPokemon(pokemonName)
-      .then(pokemonData =>  setPokemon(pokemonData))
-      .catch(error => setError(error))
+  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
+  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
+  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
+  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
+  //   fetchPokemon('Pikachu').then(
+  //     pokemonData => { /* update all the state here */},
+  //   )
+  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
+  //   1. no pokemonName: 'Submit a pokemon'
+  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
+  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
 
-  }, [pokemonName])
-  
-  
-  if (!pokemonName) {
-    return 'Submit a pokemon'
-  }
-
-  if (pokemonName && !pokemon) {
-    return (
-      <React.Fragment>
-        {error ? (
-          <div role="alert">
-          There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-          </div>
-        ): <PokemonInfoFallback name={pokemonName} />}
-      </React.Fragment>
-    )
-  }
-  
-  return <PokemonDataView pokemon={pokemon} />
-  
+  // 💣 remove this
+  return 'TODO'
 }
 
 function App() {
