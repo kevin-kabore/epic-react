30a2c2695bc0da1e4f4a487838fe0af6931dae92
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-03-19 13:19:47 -0700

    fix(advanced-hook): submodule, update countReducer 01

---
+++ advanced-hooks/src/exercise/01.js
@@ -0,0 +1,22 @@
+// useReducer: simple Counter
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+function countReducer(state, newState) {
+  
+  return state + newState;
+}
+function Counter({initialCount = 0, step = 1}) {
+  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  // const setCount = React.useCallback(step => dispatch(step + count))
+  const increment = () => setCount(step)
+  return <button onClick={increment}>{count}</button>
+}
+
+
+
+function App() {
+  return <Counter />
+}
+
+export default App
