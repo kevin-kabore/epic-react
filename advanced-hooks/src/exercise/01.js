40ae0353f7da183e7e152de16959c5aa9fe80f4c
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-03-19 16:20:52 -0700

    refactor(advanced-hooks/01): add traditional dispatch object with a type and switch statement

--- advanced-hooks/src/exercise/01.js
+++ advanced-hooks/src/exercise/01.js
@@ -2,18 +2,27 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react';
-const countReducer = (state, action) => ({
-  ...state, // always spread the state
-  ...(typeof action === 'function' ? action(action) : action),
-})
-  typeof action === 'function' ? action(state) : ({ ...state, ...action });
+
+const INCREMENT = 'INCREMENT';
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + action.step,
+      }
+    default: 
+      return state
+  }
+}
 
 function Counter({ initialCount = 0, step = 1 }) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   });
-  const {count} = state
-  const increment = () => setState((currentState) => ({ count: currentState.count + step }));
+  const { count } = state;
+  const increment = () => dispatch({ type: INCREMENT, step })
   return <button onClick={increment}>{count}</button>;
 }
 
