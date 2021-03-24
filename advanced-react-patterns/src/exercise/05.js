8933df1cc2481117f5a12722c3d1e3029ed48ffc
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-03-24 16:44:33 -0700

    feat(advanced-patterns): use actionTypes to minimize API user errors

--- advanced-react-patterns/src/exercise/05.js
+++ advanced-react-patterns/src/exercise/05.js
@@ -4,14 +4,19 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+const actionTypes = {
+  toggle: 'toggle',
+  reset: 'reset',
+}
+
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
 
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case actionTypes.toggle: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case actionTypes.reset: {
       return initialState
     }
     default: {
@@ -25,8 +30,8 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: actionTypes.toggle})
+  const reset = () => dispatch({type: actionTypes.reset, initialState})
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
@@ -51,13 +56,15 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
     getResetterProps,
   }
 }
+// export {useToggle, toggleReducer, actionTypes}
 
+// import {useToggle, toggleStateReducer, actionTypes}
 function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    if (action.type === 'toggle' && timesClicked >= 4) {
+    if (action.type === actionTypes.toggle && clickedTooMuch) {
       return {on: state.on}
     }
     return toggleReducer(state, action)
